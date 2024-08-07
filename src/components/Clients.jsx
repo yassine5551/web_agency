82127ef457315bd10923e2c0efcf399544601d40
elkoujjer yassine <yassin123beta@gmail.com>
2024-08-07 21:58:17 +0100

    clients section responsive

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -9,19 +9,19 @@ import client7 from "../assets/client7.png"
 const Clients = () => {
   return (
     <>
-    <div className='flex flex-col justify-center gap-9 items-center bg-white h-[30vh]'>
+    <div className='flex flex-col justify-center gap-9 items-center bg-white h-[50vh]'>
       <div className='flex justify-center items-center flex-col'>
         <h1 className='text-secondary text-3xl font-medium mb-[6px]'>Our Clients</h1>
-        <p className='text-secondary font-light '>We have been working with some Fortune 500+ clients</p>
+        <p className='text-secondary font-light text-center'>We have been working with some Fortune 500+ clients</p>
         </div>
-      <div className='flex justify-evenly items-center'>
+      <div className='flex justify-evenly items-center gap-5 md:gap-0'>
         <img src={client1} alt="" className=''/>
-        <img src={client2} alt="" className='ml-[10rem]'/>
-        <img src={client3} alt="" className='ml-[10rem]'/>
-        <img src={client4} alt="" className='ml-[10rem]'/>
-        <img src={client5} alt="" className='ml-[10rem]'/>
-        <img src={client6} alt="" className='ml-[10rem]'/>
-        <img src={client7} alt="" className='ml-[10rem]'/>
+        <img src={client2} alt="" className='md:ml-[10rem]'/>
+        <img src={client3} alt="" className='md:ml-[10rem]'/>
+        <img src={client4} alt="" className='md:ml-[10rem]'/>
+        <img src={client5} alt="" className='md:ml-[10rem]'/>
+        <img src={client6} alt="" className='md:ml-[10rem]'/>
+        <img src={client7} alt="" className='md:ml-[10rem]'/>
       </div>
     </div>
     </>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,15 +3,15 @@ import hero from '../assets/hero.png'
 const Hero = () => {
   return (
     <>
-      <div className='pl-[1.5rem] bg-neutralSilver flex justify-around items-center h-[100vh]'>
-        <div className='w-[60%]'>
-          <h1 className=' text-7xl font-medium tracking-normal text-secondary mb-[2rem]'>Lessons and insights</h1>
-          <span className='text-shade text-7xl font-medium tracking-normal '>from 8 years</span>
+      <div className='pl-[1.5rem] bg-neutralSilver flex flex-col-reverse md:flex-row justify-around items-center h-[100vh] mt-[32px] '>
+        <div className='md:w-[60%] w-[100%]'>
+          <h1 className=' md:text-7xl text-5xl font-medium tracking-normal text-secondary mb-[2rem]'>Lessons and insights</h1>
+          <span className='text-shade md:text-7xl text-5xl font-medium tracking-normal '>from 8 years</span>
           <p className='text-secondary mt-[2rem] font-light'>where to grow your business as a photographer: site or social media?</p>
           <button className='mt-[2rem] bg-none border-none bg-shade hover:bg-green px-[2.2rem] py-[1rem] text-white rounded-md'>Register</button>
         </div>
         
-          <img src={hero} alt=""  className='w-[391px] h-[407px] object-cover'/> 
+          <img src={hero} alt=""  className='md:w-[391px] md:h-[407px] w-[400px] h-[280px] object-contain md:object-cover'/> 
         
       </div>
     </>
