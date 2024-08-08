d499808394fe947a2d5d08cc1716dc536804565c
elkoujjer yassine <yassin123beta@gmail.com>
2024-08-08 01:39:02 +0100

    adding animations

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,47 +4,96 @@ import { faX } from "@fortawesome/free-solid-svg-icons";
 import React from "react";
 import logo from "../assets/logo.png";
 import { useState } from "react";
+import { delay, easeIn, motion, stagger } from "framer-motion";
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
   const toggleNav = () => {
     setIsOpen(!isOpen);
   };
+  const variantNav = {
+    hidden :{
+      y:-100,
+      opacity:0
+    },
+    visible : {
+      y:0,
+      opacity:1,
+      transition :{
+        duration:1.1,
+        ease:"easeIn",
+        staggerChildren:0.4
+      }
+    }
+  }
+  const variantChildren = {
+    hidden :{
+      opacity :0,
+      x:-80,
+      
+    },
+    visible:{
+      opacity:1,
+      x:0,
+      transition:{
+        duration:1.5,
+        when:'beforeChildren',
+        delay:1,
+        
+      }
+    }
+  }
+  const variantLinks= {
+    hidden :{
+      opacity:0,
+      y:-50
+    },
+    visible :{
+      opacity:1,
+      y:0,
+      transition :{
+        duration:2.5,
+        delay:1,
+        ease:'easeIn'
+      }
+    }
+    
+  }
   return (
     <>
-      <nav className="z-[10000] bg-neutralSilver flex justify-between items-center py-[2rem] px-[2.5rem] fixed top-0 w-full h-[32px]   ">
-        <div>
+      <motion.nav variants={variantNav} initial="hidden" animate="visible"  className="z-[10000] bg-neutralSilver flex justify-between items-center py-[2rem] px-[2.5rem] fixed top-0 w-full h-[32px]   ">
+        <motion.div variants={variantChildren} initial="hidden" animate="visible">
           <img src={logo} alt="" />
-        </div>
+        </motion.div>
 
-        <ul className="md:flex gap-[2rem] hidden">
+        <motion.ul variants={variantChildren} initial='hidden' animate="visible" className="md:flex gap-[2rem] hidden">
           <li className="cursor-pointer hover:text-shade transition-all font-semibold">
             Home
           </li>
-          <li className="cursor-pointer hover:text-shade transition-all">
+          <li  className="cursor-pointer hover:text-shade transition-all">
             Service
           </li>
-          <li className="cursor-pointer hover:text-shade transition-all">
+          <li  className="cursor-pointer hover:text-shade transition-all">
             Feature
           </li>
-          <li className="cursor-pointer hover:text-shade transition-all">
+          <li  className="cursor-pointer hover:text-shade transition-all">
             Product
           </li>
-          <li className="cursor-pointer hover:text-shade transition-all">
+          <li  className="cursor-pointer hover:text-shade transition-all">
             Testimonial
           </li>
-          <li className="cursor-pointer hover:text-shade transition-all">
+          <li  className="cursor-pointer hover:text-shade transition-all">
             FAQ
           </li>
-        </ul>
+        </motion.ul>
 
-        <div className="md:flex hidden">
+        <motion.div initial={{opacity:0,scale:1.1}} animate={{opacity:1,scale:1,transition:{duration:2,delay:1.2  }}} className="md:flex hidden">
           <button className="bg-none border-none md:px-[20px] px-3 py-1 md:py-[10px] text-shade">
             Login
           </button>
           <button className="bg-shade md:px-[20px] px-3 py-1 md:py-[10px] rounded-md hover:bg-green transition-all text-white ">
             Sign up
           </button>
-        </div>
+        </motion.div>
         <div onClick={toggleNav} className="md:hidden">
           {isOpen ? (
             <FontAwesomeIcon icon={faX} className="text-2xl text-shade" />
@@ -52,7 +101,7 @@ const Header = () => {
             <FontAwesomeIcon icon={faBars} className="text-2xl text-shade" />
           )}
         </div>
-      </nav>
+      </motion.nav>
 
       {isOpen && (
         <div className="w-full h-[100vh] flex justify-start  items-center pl-5 gap-[65px] bg-shade fixed z-50">
@@ -77,13 +126,13 @@ const Header = () => {
             </li>
           </ul>
           <div className="flex flex-col gap-6 ">
-          <button className="bg-neutralSilver hover:bg-info transition-all font-semibold hover:text-white rounded-md border-none md:px-[20px] px-6 py-3 md:py-[10px] text-secondary">
-            Login
-          </button>
-          <button className="bg-info md:px-[20px] font-semibold hover:bg-neutralSilver h px-6 py-3 md:py-[10px] rounded-md hover:text-info transition-all text-white ">
-            Sign up
-          </button>
-        </div>
+            <button className="bg-neutralSilver hover:bg-info transition-all font-semibold hover:text-white rounded-md border-none md:px-[20px] px-6 py-3 md:py-[10px] text-secondary">
+              Login
+            </button>
+            <button className="bg-info md:px-[20px] font-semibold hover:bg-neutralSilver h px-6 py-3 md:py-[10px] rounded-md hover:text-info transition-all text-white ">
+              Sign up
+            </button>
+          </div>
         </div>
       )}
     </>
