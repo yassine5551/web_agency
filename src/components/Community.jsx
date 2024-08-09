4fc5a6c85b9b2236e3b025ff8f260ed30198ed14
elkoujjer yassine <yassin123beta@gmail.com>
2024-08-09 01:16:16 +0100

    adding more animations

--- src/components/Community.jsx
+++ src/components/Community.jsx
@@ -3,14 +3,40 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUsers } from "@fortawesome/free-solid-svg-icons";
 import { faBuilding } from "@fortawesome/free-solid-svg-icons";
 import { faHandHoldingHand } from "@fortawesome/free-solid-svg-icons";
+import { motion } from "framer-motion";
+
+export const fadeUp = (delay) => {
+  return {
+    initial: {
+      y: 50,
+      opacity: 0,
+    },
+    animate: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        type: "spring",
+        stiffness: 100,
+        duration: 0.5,
+        delay: delay,
+      },
+    },
+  };
+};
 const Community = () => {
   return (
     <>
-      <div className="flex flex-col justify-center gap-9 items-center bg-white h-[100vh] mb-[250px] md:mb-0">
+      <motion.div
+        variants={fadeUp(0.8)}
+        initial="initial"
+        whileInView="animate"
+        viewport={{ once: true }}
+        className="flex flex-col justify-center gap-9 items-center bg-white h-[100vh] mb-[250px] md:mb-0"
+      >
         <div className="flex justify-center items-center flex-col ">
           <h1 className="text-secondary md:text-3xl text-4xl font-semibold text-center">
             Manage your entire community
-          </h1> 
+          </h1>
           <p className="text-secondary md:text-3xl text-4xl font-semibold mb-[6px]">
             in a single system
           </p>
@@ -20,7 +46,23 @@ const Community = () => {
         </div>
 
         <div className="flex justify-center items-center gap-[120px]  flex-col md:flex-row">
-          <div className="w-[250px] h-[250px]  flex justify-center items-center flex-col shadow-primary shadow-xl rounded-xl">
+          <motion.div
+            initial={{ x: -100, opacity: 0 }}
+            whileInView={{
+              x: 0,
+              opacity: 1,
+
+              transition: {
+                duration: 0.3,
+                delay: 0.6,
+                type: "spring",
+                stiffness: 100,
+                ease: "easeInOut",
+              },
+            }}
+            whileHover={{scale:1.1,transition:{duration:0.3}}}
+            className="w-[250px] h-[250px]  flex justify-center items-center flex-col shadow-primary shadow-xl rounded-xl"
+          >
             <div className="bg-lightGreen w-[35px] h-[35px] rounded-br-2xl rounded-sm relative mb-[20px]">
               <FontAwesomeIcon
                 icon={faUsers}
@@ -34,9 +76,9 @@ const Community = () => {
               Our membership management software provides full automation of
               membership renewals and payments
             </p>
-          </div>
+          </motion.div>
 
-          <div className="w-[250px] h-[250px]  flex justify-center items-center flex-col flex-wrap shadow-primary shadow-xl rounded-xl">
+          <motion.div whileHover={{scale:1.1,transition:{duration:0.3}}} className="w-[250px] h-[250px]  flex justify-center items-center flex-col flex-wrap shadow-primary shadow-xl rounded-xl">
             <div className="bg-lightGreen  w-[35px] h-[35px] rounded-br-2xl rounded-sm relative mb-[20px]">
               <FontAwesomeIcon
                 icon={faBuilding}
@@ -50,9 +92,25 @@ const Community = () => {
               Our membership management software provides full automation of
               membership renewals and payments
             </p>
-          </div>
+          </motion.div>
 
-          <div className="w-[250px] h-[250px]  flex justify-center items-center flex-col flex-wrap  shadow-primary shadow-xl rounded-xl">
+          <motion.div
+            initial={{ x: 100, opacity: 0 }}
+            whileInView={{
+              x: 0,
+              opacity: 1,
+
+              transition: {
+                duration: 0.3,
+                delay: 0.6,
+                type: "spring",
+                stiffness: 100,
+                ease: "easeInOut",
+              },
+            }}
+            whileHover={{scale:1.1,transition:{duration:0.3}}}
+            className="w-[250px] h-[250px]  flex justify-center items-center flex-col flex-wrap  shadow-primary shadow-xl rounded-xl"
+          >
             <div className="bg-lightGreen w-[35px] h-[35px] rounded-br-2xl rounded-sm relative mb-[20px]">
               <FontAwesomeIcon
                 icon={faHandHoldingHand}
@@ -66,9 +124,9 @@ const Community = () => {
               Our membership management software provides full automation of
               membership renewals and payments
             </p>
-          </div>
+          </motion.div>
         </div>
-      </div>
+      </motion.div>
     </>
   );
 };
